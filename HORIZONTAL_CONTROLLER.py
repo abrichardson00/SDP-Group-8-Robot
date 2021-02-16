80de892a57054ed35ac2a75f2be6e90f57f20ccb
BlairRenX <39006447+BlairRenX@users.noreply.github.com>
2021-02-16 16:36:08 +0000

    Add files via upload
    Controller has 2 modes: 'Server' (for main use) and 'Keyboard' for testing purposes. Server mode accepts vertical and horizontal height targets and will then move towards then. In/out movement is not yet functional, but hopefully will be soon.

--- HORIZONTAL_CONTROLLER.py
+++ HORIZONTAL_CONTROLLER.py
@@ -1,26 +1,39 @@
 """HORIZONTAL_CONTROLLER controller."""
+global keyboardMode
+global serverMode
+keyboardMode = True
+serverMode = False
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Motor, Keyboard, Brake
+from controller import Robot, Motor, Keyboard, Brake, PositionSensor
 
 # create the Robot instance.
 robot = Robot()
 
-speed = 0.5
+speed = 0.1
 rackAndPinionV = robot.getDevice('vertical linear motor')
 rackAndPinionH = robot.getDevice('horizontal linear motor')
+frontBackMotor = robot.getDevice('FrontBackSelector')
+
+vertPosSensor = robot.getDevice('VertPos')
+vertPosSensor.enable(200)
+horiPosSensor = robot.getDevice('HoriPos')
+horiPosSensor.enable(200)
 
 
 rackAndPinionH.setPosition(0)
 rackAndPinionV.setPosition(0)
+frontBackMotor.setPosition(0)
 #.setPosition(10.0)
 rackAndPinionH.setPosition(float('inf'))
+rackAndPinionV.setPosition(float('inf'))
 rackAndPinionV.setPosition(float('inf'))
 #vertRack.setPosition(float('inf'))
 #vertRack.setVelocity(speed)
 rackAndPinionH.setVelocity(0)
 rackAndPinionV.setVelocity(0)
+frontBackMotor.setVelocity(0)
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
@@ -29,6 +42,86 @@ timestep = int(robot.getBasicTimeStep())
 keyboard = Keyboard()
 keyboard.enable(timestep)
 
+
+
+def keyboardMotion():
+   vVal = 0
+   hVal = 0
+   fbVal = 0
+   key = keyboard.getKey()
+   if (key == Keyboard.UP):
+        #breakV.setDampingConstant(0.0)
+       #rackAndPinionV.setVelocity(speed)
+       vVal = speed
+   elif (key == Keyboard.DOWN):
+        #breakV.setDampingConstant(0.0)
+     #rackAndPinionV.setVelocity(-1 * speed)
+     vVal = -speed
+   else:
+      #rackAndPinionV.setVelocity(0)
+      vVal = 0
+        
+   if (key == Keyboard.RIGHT):
+        #breakH.setDampingConstant(0.0)
+      #rackAndPinionH.setVelocity(speed)
+      hVal = speed
+   elif(key == Keyboard.LEFT):
+        #breakH.setDampingConstant(0.0)
+      hVal = -speed
+      #rackAndPinionH.setVelocity(-1 * speed)
+   else:
+      hVal = 0
+      #rackAndPinionH.setVelocity(0)
+      
+   if (key == ord('W')):
+      fbVal = speed
+   elif (key == ord('S')):
+      fbVal = -speed
+   else:
+      fbVal = 0
+      
+   return [vVal, hVal, fbVal]
+      
+      
+def serverMotion():
+   #define how the robot should receive packets and commands from the server
+   targetVertVal = 0.6
+   targetHoriVal = 0.6
+   vVal = 0
+   hVal = 0
+   frontBack = 1 #1 for front, 0 for back
+   """
+   if vertPosSensor.getValue() < targetVertVal:
+      #rackAndPinionV.setVelocity(speed)
+      vVal = 1
+   elif vertPosSensor.getValue() > targetVertVal:
+      #rackAndPinionV.setVelocity(-1 * speed)
+      vVal = -1
+   else:
+      vVal = 0
+      #rackAndPinionV.setVelocity(0)
+      """
+      
+   vVal = (targetVertVal - vertPosSensor.getValue())*0.5
+   if round(vVal, 3) == 0:
+      vVal = 0  
+      
+   """
+   if horiPosSensor.getValue() < targetHoriVal:
+      #rackAndPinionV.setVelocity(speed)
+      hVal = 1
+   elif vertPosSensor.getValue() > targetVertVal:
+      #rackAndPinionV.setVelocity(-1 * speed)
+      hVal = -1
+   else:
+      hVal = 0
+      #rackAndPinionV.setVelocity(0)
+   """
+   hVal = (targetHoriVal - horiPosSensor.getValue())*0.5
+   if round(hVal, 3) == 0:
+      hVal = 0
+   return [vVal, hVal]
+
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
 #  motor = robot.getMotor('motorname')
@@ -36,28 +129,20 @@ keyboard.enable(timestep)
 #  ds.enable(timestep)
 
 # Main loop:
+v,h, fb = 0,0,0
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    key = keyboard.getKey()
-    if (key == Keyboard.UP):
-        #breakV.setDampingConstant(0.0)
-        rackAndPinionV.setVelocity(speed)
-    elif (key == Keyboard.DOWN):
-        #breakV.setDampingConstant(0.0)
-        rackAndPinionV.setVelocity(-1 * speed)
-    else:
-        rackAndPinionV.setVelocity(0)
-        
-    if (key == Keyboard.RIGHT):
-        #breakH.setDampingConstant(0.0)
-        rackAndPinionH.setVelocity(speed)
-    elif(key == Keyboard.LEFT):
-        #breakH.setDampingConstant(0.0)
-        rackAndPinionH.setVelocity(-1 * speed)
-    else:
-        rackAndPinionH.setVelocity(0)
-        
-        
+   print(vertPosSensor.getValue())
+   if serverMode == True:
+      v,h = serverMotion()
+   elif keyboardMode == True:
+      v,h,fb = keyboardMotion()
+   else:
+      pass
+      
+   rackAndPinionV.setVelocity(v)
+   rackAndPinionH.setVelocity(h)
+   frontBackMotor.setVelocity(fb)
     #if(key != Keyboard.LEFT || key != Keyboard.RIGHT):
         #breakH.setDampingConstant(10.0)
     # Read the sensors:
@@ -68,6 +153,8 @@ while robot.step(timestep) != -1:
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
-    pass
+
+
+
 
 # Enter here exit cleanup code.
