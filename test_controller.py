1f5d4e67729d3b397ac44431313e97877b0107dd
BlairRenX <39006447+BlairRenX@users.noreply.github.com>
2021-02-25 17:46:31 +0000

    Add files via upload
    Updated controller file. Can perform retrieve operations autonomously now.

--- test_controller.py
+++ test_controller.py
@@ -1,8 +1,8 @@
 """HORIZONTAL_CONTROLLER controller."""
 global keyboardMode
 global serverMode
-keyboardMode = True
-serverMode = False
+keyboardMode = False
+serverMode = True
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
@@ -21,6 +21,8 @@ vertPosSensor = robot.getDevice('VertPos')
 vertPosSensor.enable(200)
 horiPosSensor = robot.getDevice('HoriPos')
 horiPosSensor.enable(200)
+grabSensor = robot.getDevice('grabberPosSensor')
+grabSensor.enable(200)
 
 
 rackAndPinionH.setPosition(0)
@@ -52,6 +54,10 @@ timestep = int(robot.getBasicTimeStep())
 
 keyboard = Keyboard()
 keyboard.enable(timestep)
+
+def supervise(): #give the robot a command with this function
+#TODO: accept enter/retrive commands. maybe make a custom class for it?
+    return ["Retrieve", 4, "R", "F"] #dummy result. Retrieve from the forward stack, 4th row, right column
 
 
 
@@ -94,44 +100,27 @@ def keyboardMotion():
    return [vVal, hVal, fbVal]
       
       
-def serverMotion():
+def serverMotion(targetVertVal, targetHoriVal, targetFbVal):
    #define how the robot should receive packets and commands from the server
-   targetVertVal = 0.6
-   targetHoriVal = 0.6
    vVal = 0
    hVal = 0
-   frontBack = 1 #1 for front, 0 for back
-   """
-   if vertPosSensor.getValue() < targetVertVal:
-      #rackAndPinionV.setVelocity(speed)
-      vVal = 1
-   elif vertPosSensor.getValue() > targetVertVal:
-      #rackAndPinionV.setVelocity(-1 * speed)
-      vVal = -1
-   else:
-      vVal = 0
-      #rackAndPinionV.setVelocity(0)
-      """
+   fbVal = 0 #from -0.2 to 0.2
       
-   vVal = (targetVertVal - vertPosSensor.getValue())*0.5
+   vVal = (targetVertVal - vertPosSensor.getValue())*0.7
    if round(vVal, 3) == 0:
       vVal = 0  
       
-   """
-   if horiPosSensor.getValue() < targetHoriVal:
-      #rackAndPinionV.setVelocity(speed)
-      hVal = 1
-   elif vertPosSensor.getValue() > targetVertVal:
-      #rackAndPinionV.setVelocity(-1 * speed)
-      hVal = -1
-   else:
+   hVal = (targetHoriVal - horiPosSensor.getValue())*0.7
+   if vVal != 0 or round(hVal, 3) == 0:
       hVal = 0
-      #rackAndPinionV.setVelocity(0)
-   """
-   hVal = (targetHoriVal - horiPosSensor.getValue())*0.5
-   if round(hVal, 3) == 0:
-      hVal = 0
-   return [vVal, hVal]
+      
+      
+   fbVal = (targetFbVal - grabSensor.getValue())*0.5
+   if vVal != 0 or hVal != 0 or round(fbVal, 3) == 0:
+       fbVal = 0
+  
+   print([vVal, hVal, fbVal])
+   return [vVal, hVal, fbVal]
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -139,13 +128,41 @@ def serverMotion():
 #  ds = robot.getDistanceSensor('dsname')
 #  ds.enable(timestep)
 
+def retrieve(): #we have to use generators due to timestep shenanigans
+   tgv, tgh, tgfb = 0.53, -0.21, 0.166
+   while round(vertPosSensor.getValue(), 2) != 0.68 or horiPosSensor.getValue() != 0 or grabSensor.getValue() != 0:
+      
+      yield [0.68,0,0] #set it to a start position.
+   #the robot is now at the start position
+   while serverMotion(tgv, tgh, tgfb) != [0,0,0]:
+      yield [tgv, tgh, tgfb]
+      
+   while serverMotion(tgv, tgh, 0) != [0, 0, 0]:
+       print("stage: retriever return")
+       yield [tgv, tgh, 0] #return the retriever to rest pos
+       
+   while serverMotion(tgv, 0, 0) != [0, 0, 0]:
+       yield [tgv, 0, 0]
+   
+   while vertPosSensor.getValue() != 1 or horiPosSensor.getValue() != 0 or grabSensor.getValue() != 0:
+      yield [0.68,0,0] #set it to a start position.    
+    
+
+
+gen = retrieve()
+
+
+
 # Main loop:
+#tgv, tgh, tgfb = 0.53, -0.21, 0.2
 v,h, fb = 0,0,0
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
    #print(vertPosSensor.getValue())
    if serverMode == True:
-      v,h = serverMotion()
+      tgv, tgh, tgfb = next(gen)
+      print([vertPosSensor.getValue(), horiPosSensor.getValue(), grabSensor.getValue()])
+      v,h, fb = serverMotion(tgv, tgh, tgfb)
    elif keyboardMode == True:
       v,h,fb = keyboardMotion()
    else:
