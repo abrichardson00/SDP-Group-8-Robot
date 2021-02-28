edfb55306d8bfb0af6784903e62331819ae180c3
AdamBlance <adam.blance@gmail.com>
2021-02-28 21:13:12 +0000

    Implement retrieval functionality

--- controllers/server2/server2.py
+++ controllers/server2/server2.py
@@ -5,6 +5,7 @@ from threading import Thread
 from time import sleep
 from functools import partial
 from sys import stdout
+from enum import Enum
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,12 +14,32 @@ tray_regex_parser = reqparse.RequestParser()
 # tray_regex_parser.add_argument("tray", type=inputs.regex("(B|F)(L|R)[0-4]"), required=True, strict=True)
 
 CONTROL_STEP = 64
-target_height = -1
-target_grabber = -1
+# target_height = -1
+# target_grabber = -1
+
+class State(Enum):
+    IDLE = 1
+    MOVING_TO_TRAY = 2
+    EXTENDING_GRABBER = 3
+    PICKING_UP_TRAY = 4
+    RETRACTING_GRABBER = 5
+    MOVING_TO_TOP = 6
+
+system_state = State.IDLE
+vertical_target = -1
+horizontal_target = -1
+left_grabber_target = -1
 
 @api.route("/retrieve/<string:tray>")
 class Retrieve(Resource):
-    def put(self, tray):
+    def get(self, tray):
+
+        global system_state
+        system_state = State.MOVING_TO_TRAY
+
+        global vertical_target
+        vertical_target = 0.01 + int(tray[2])*0.13
+
         return {"yay": tray}
 
 
@@ -38,13 +59,22 @@ class GrabberResource(Resource):
         target_grabber = float(value)
         return {"hello": "world"}
 
+def in_range(position, target):
+    return target - 0.0001 < position < target + 0.0001
+
 if __name__ == "__main__":
 
     theostore = Robot()
+    
     vertical_motor = theostore.getDevice("vertical motor")
+    
     vertical_motor_pos_sensor = theostore.getDevice("VertPos")
     vertical_motor_pos_sensor.enable(CONTROL_STEP)
+    
     left_grabber_motor = theostore.getDevice("left grabber motor")
+    
+    left_grabber_motor_pos_sensor = theostore.getDevice("left_grabber_sensor")
+    left_grabber_motor_pos_sensor.enable(CONTROL_STEP)
 
     print("Giving Flask a second to boot up...")
 
@@ -59,13 +89,37 @@ if __name__ == "__main__":
         sleep(0.01)
 
     while theostore.step(CONTROL_STEP) != -1:
-        # print("webots target pos = %s" % target_pos)
-        if target_height != -1:
-            print(vertical_motor_pos_sensor.getValue())
-            # print("trying to move")
-            # vertical_motor.setPosition(target_pos)
-            vertical_motor.setPosition(target_height)
-        if target_grabber != -1:
-            left_grabber_motor.setPosition(target_grabber)
+        if system_state == State.MOVING_TO_TRAY:
+            vertical_motor.setPosition(vertical_target)
+            current_pos = vertical_motor_pos_sensor.getValue()
+            if in_range(current_pos, vertical_target):
+                system_state = State.EXTENDING_GRABBER
+        
+        elif system_state == State.EXTENDING_GRABBER:
+            left_grabber_motor.setPosition(0.22)
+            current_pos = left_grabber_motor_pos_sensor.getValue()
+            if in_range(current_pos, 0.22):
+                system_state = State.PICKING_UP_TRAY
 
+        elif system_state == State.PICKING_UP_TRAY:
+            vertical_motor.setPosition(vertical_target + 0.01)
+            current_pos = vertical_motor_pos_sensor.getValue()
+            if in_range(current_pos, vertical_target + 0.01):
+                system_state = State.RETRACTING_GRABBER
+        
+        elif system_state == State.RETRACTING_GRABBER:
+            left_grabber_motor.setPosition(0.0)
+            current_pos = left_grabber_motor_pos_sensor.getValue()
+            if in_range(current_pos, 0.0):
+                system_state = State.MOVING_TO_TOP
+        
+        elif system_state == State.MOVING_TO_TOP:
+            vertical_motor.setPosition(0.65)
+            current_pos = vertical_motor_pos_sensor.getValue()
+            if in_range(current_pos, 0.65):
+                system_state = State.IDLE
+        
+        
+
+            
     
